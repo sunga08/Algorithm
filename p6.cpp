c3fe4e727413931f79410db8a637b41c8ad1e403
sunga08 <eert2260@naver.com>
2021-01-03 23:02:01 +0900

    upload_210103

---
+++ p6.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+int main()
+{
+    char a[51];
+    int res=0, cnt=0;
+    scanf("%s", &a);
+    for (int i = 0; a[i] != '\0'; i++) {
+        if (a[i] >= 48 && a[i] <= 57) {
+            res = res * 10 + (a[i] - 48);
+        }
+    }
+    printf("%d \n", res);
+
+    for (int i = 1; i <= res; i++) {
+        if (res % i == 0) cnt++;
+    }
+    printf("%d \n", cnt);
+
+    return 0;
+}
+
