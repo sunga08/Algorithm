1d601078e14779d60c2635387218cbfef6dc9eeb
sunga08 <eert2260@naver.com>
2021-01-04 23:33:28 +0900

    upload_210104

---
+++ code/p8.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    char a[31];
+    int cnt = 0;
+    scanf("%s", &a);
+    for (int i = 0; a[i] != '\0'; i++) {
+        if (a[i] == '(') cnt++;
+        else if (a[i] == ')') cnt--;
+        if (cnt < 0) break;
+    }
+
+    if (cnt == 0) printf("YES");
+    else printf("NO");
+
+    return 0;
+}
+
+
---
+++ code/p9.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+
+int a[50001];
+int main()
+{
+    int N;
+    scanf("%d", &N);
+    for (int i = 1; i <= N; i++) {
+        for (int j = i; j <= N; j = j + i) {
+            a[j]++;
+        }
+    }
+
+    for (int i = 1; i <= N; i++) {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
+
