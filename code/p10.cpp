7d657282d01172fdaa48efcb7298306df5d33512
sunga08 <eert2260@naver.com>
2021-01-05 00:26:49 +0900

    upload_210104

---
+++ code/p10.cpp
@@ -0,0 +1,35 @@
+﻿
+#include <iostream>
+
+int digit_sum(int x) {
+    int sum = 0;
+
+    while (x > 0) {
+        sum += x % 10;
+        x = x / 10;
+    }
+    return sum;
+}
+
+int main()
+{
+    int N, num, res, sum=0, max=-2147000000;
+    scanf("%d", &N);
+
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &num);
+        sum = digit_sum(num);
+        if (sum > max) {
+            max = sum;
+            res = num;
+        }
+        else if (sum == max) {
+            if (num > res) res = num;
+        }
+    }
+    printf("%d", res);
+
+    return 0;
+}
+
+
