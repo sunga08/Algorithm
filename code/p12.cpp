31ff898d4aed78a04232675d10bb62bdd251759f
sunga08 <eert2260@naver.com>
2021-01-05 23:55:24 +0900

    solved_210105

--- code/p12.cpp
+++ code/p12.cpp
@@ -1,18 +1,36 @@
 ﻿#include <iostream>
 
-int main()
+int main(void) {
+	int n, sum = 0, c = 1, d = 9, res = 0;
+	scanf("%d", &n);
+	while (sum + d < n) {
+		res = res + (c * d);
+		sum = sum + d;
+		c++;
+		d = d * 10;
+	}
+
+	res = res + ((n - sum) * c);
+	printf("%d", res);
+
+	return 0;
+}
+
+
+//내 풀이
+/*int main()
 {
-	int N, i=10, j=1, cnt=0;
+	int N, i = 10, j = 1, cnt = 0;
 	scanf("%d", &N);
-	
+
 	while (true) {
-		if (N >= i-1) {
-			cnt += j * 9 * (i/10);
+		if (N >= i - 1) {
+			cnt += j * 9 * (i / 10);
 			i = i * 10;
 			j++;
 		}
 		else if (N < i - 1) {
-			cnt += (N - ((i / 10) - 1))* j;
+			cnt += (N - ((i / 10) - 1)) * j;
 			break;
 		}
 	}
@@ -20,4 +38,5 @@ int main()
 	printf("%d", cnt);
 
 	return 0;
-}
+}*/
+
