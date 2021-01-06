55529adef2055343d87373a8cb80a954aa464d1d
sunga08 <eert2260@naver.com>
2021-01-06 23:49:25 +0900

    solved_210106

---
+++ code/baseballGame.cpp
@@ -0,0 +1,86 @@
+﻿#include <iostream>
+
+int main()
+{
+    int input, i1, i2, i3;
+    int answer, a1, a2, a3;
+    int strike = 0, ball = 0;
+
+    scanf("%d", &input);
+    i1 = input / 100;
+    i2 = input % 100 / 10;
+    i3 = input % 100 % 10;
+
+    while (strike != 3) {
+        strike = 0, ball = 0;
+        scanf("%d", &answer);
+        a1 = answer / 100;
+        a2 = answer % 100 / 10;
+        a3 = answer % 100 % 10;
+
+        if (a1 == i1) {
+            strike++;
+        }
+        else if (a1 == i2 || a1 == i3) {
+            ball++;
+        }
+
+        if (a2 == i2) {
+            strike++;
+        }
+        else if (a2 == i1 || a1 == i3) {
+            ball++;
+        }
+
+        if (a3 == i3) {
+            strike++;
+        }
+        else if (a3 == i1 || a3 == i2) {
+            ball++;
+        }
+
+        printf("%dstrike %dball", strike, ball);
+    }
+
+
+    /*int n1, n2, n3;
+    int a1, a2, a3;
+    int strike=0, ball = 0;
+
+    scanf("%d %d %d", &n1, &n2, &n3);
+
+    while (strike!=3) {
+        strike = 0, ball = 0;
+        scanf("%d %d %d", &a1, &a2, &a3);
+        
+        if (a1 == n1) {
+            strike++;
+        }
+        else if (a1 == n2 || a1 == n3) {
+            ball++;
+        }
+
+        if (a2 == n2) {
+            strike++;
+        }
+        else if (a2 == n1 || a1 == n3) {
+            ball++;
+        }
+
+        if (a3 == n3) {
+            strike++;
+        }
+        else if (a3 == n1 || a3 == n2) {
+            ball++;
+        }
+
+        printf("%dstrike %dball", strike, ball);
+    }*/
+    
+
+
+
+    return 0;
+}
+
+
---
+++ code/p13.cpp
@@ -0,0 +1,32 @@
+﻿
+#include <iostream>
+
+//자연수 최대 길이는 100이므로 int에 저장 못함 => 배열 사용(문자열로 입력 받음)
+//ASCII코드 사용해서 문자열 -> 숫자로 바꾸기
+
+int a[10];
+int main()
+{
+    char input[101];
+    int max; //최대 자릿수(인덱스)
+    int cnt_max = -1; //등장 횟수
+
+    scanf("%s", &input);
+
+    for (int i = 0; input[i] != '\0'; i++) {
+        a[input[i] - 48]++;
+    }
+
+    for (int i = 0; i < 10; i++) {
+        if (cnt_max <= a[i]) { // '='도 포함 : 0->9로 읽으면서 최댓값이 같으면 무조건 나중에 읽은 인덱스가 큼.
+            max = i;
+            cnt_max = a[i];
+        }
+
+    }
+
+    printf("%d", max);
+
+    return 0;
+}
+
