e73aab44a1b46bd937ea34f3eef4d9d68d081d52
sunga08 <eert2260@naver.com>
2021-01-10 23:48:44 +0900

    solved_210110

---
+++ code/p18.cpp
@@ -0,0 +1,27 @@
+﻿#pragma warning(disable: 4996)
+#include <iostream>
+
+int main()
+{
+    freopen("input.txt", "rt", stdin);
+    int n, m, temp, cnt=0, max = -2147000;
+    bool isAlarm=false;
+    scanf("%d %d", &n, &m);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &temp);
+        if (temp > m) {
+            cnt++;
+            isAlarm = true;
+        }
+        else cnt = 0;
+
+        if (cnt > max) max = cnt;
+    }
+
+    //if (isAlarm == false) printf("-1");
+    if (max == 0) printf("-1");
+    else printf("%d", max);
+
+    return 0;
+}
---
+++ code/p19.cpp
@@ -0,0 +1,26 @@
+﻿#pragma warning(disable: 4996)  
+#include <iostream>
+
+int arr[100];
+int main()
+{
+    freopen("input.txt", "rt", stdin);
+    int n, max=-1, cnt=0;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    max = arr[n - 1];//max를 arr[n-1]로 먼저 초기화
+
+    for (int i = n-1; i >= 0; i--) {
+        if (arr[i] > max) {
+            max = arr[i];
+            cnt++;
+        }
+    }
+
+    //printf("%d", cnt - 1);
+    printf("%d", cnt);
+
+    return 0;
+}
