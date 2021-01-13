e567b05ad388b8a7691b097e8e561c42bbfacdd6
sunga08 <eert2260@naver.com>
2021-01-14 00:49:22 +0900

    solved_210113

---
+++ code/p25.cpp
@@ -0,0 +1,31 @@
+﻿#pragma warning (disable:4996)
+#include <iostream>
+//#include <vector>
+using namespace std;
+
+int main()
+{
+    freopen("input.txt", "rt", stdin);
+    int n, idx, max = -21470000, pre=0, rank=0, cnt=0;
+    scanf("%d", &n);
+    //vector<int> v(n);
+    //vector<int> res(n);
+    int v[100];
+    int res[100];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &v[i]);
+        res[i] = 1;
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (v[i] < v[j]) res[i]++;
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", res[i]);
+    }
+
+    return 0;
+}
