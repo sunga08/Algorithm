6a462bd2c50f7c6d904fef838d1202ca7bc3fbdb
sunga08 <eert2260@naver.com>
2021-01-15 00:52:54 +0900

    solved_210114

--- code/p26.cpp
+++ code/p26.cpp
@@ -3,21 +3,22 @@
 #include <vector>
 using namespace std;
 
-//4,5번 time limit
+//데이터가 더 커지면 합병정렬 사용해야 됨
+//4,5번 time limit (Release 모드로 컴파일시 통과)
+//앞 사람들 중 자신보다 크거나 같은 사람 count => 내 등수 = count+1
 int main()
 {
     freopen("input.txt", "rt", stdin);
     int n, cnt = 0;
     scanf("%d", &n);
-    vector<int> a(n + 1);
-    //vector<int> b(n);
+    vector<int> a(n);
 
     for (int i = 0; i < n; i++) {
         scanf("%d", &a[i]);
     }
+
     printf("1 ");
     for (int i = 1; i < n; i++) {
-        //scanf("%d", &a[i]);
         cnt = 0;
         //for (int j = 0; j < i; j++) {
         for (int j = i - 1; j >= 0; j--) {
@@ -27,9 +28,6 @@ int main()
         //b[i] = cnt+1;
         printf("%d ", cnt + 1);
     }
-
-
-    
 
     return 0;
 }
