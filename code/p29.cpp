56f6bcc305f1872885744418b6437eca0f2a3f2a
Sung A <53103434+sunga08@users.noreply.github.com>
2021-01-30 10:27:12 +0900

    solved_210130

---
+++ code/p29.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+
+int main()
+{
+    //freopen("input.txt", "rt", stdin);
+    int n, cnt=0;
+    scanf("%d", &n);
+
+    //모든 자리수 확인하기
+    for (int i = 1; i <= n; i++) {
+        int tmp = i;
+        while (tmp != 0) {
+            int j = tmp % 10;
+            if (j == 3) cnt++;
+            tmp = tmp / 10;
+        }
+    }
+
+
+    printf("%d", cnt);
+    return 0;
+}
